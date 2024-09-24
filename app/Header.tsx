8f2b8d78b7a59c70b9c19eeb55212607c01daf0b
Casper Engelmann <c.engelmann@custimy.io>
2024-09-24 22:57:57 +0200

    Add onboarding reset button to header

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,6 +5,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/20/solid";
 import { BellIcon } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
+import { useOnboarding } from "~/app/use-onboarding";
 
 const navigation = [
   { name: "Home", href: "/" },
@@ -14,48 +15,64 @@ const navigation = [
 
 export function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { resetCompletedSteps } = useOnboarding([]);
 
   return (
-    <header className="absolute inset-x-0 top-0 z-50 flex h-16 border-b border-gray-900/10">
-      <div className="mx-auto flex w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-1 items-center gap-x-6">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-3 p-3 md:hidden"
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="h-5 w-5 text-gray-900" />
-          </button>
-          <img
-            alt="Your Company"
-            src="https://tailwindui.com/img/logos/mark.svg?color=violet&shade=600"
-            className="h-8 w-auto"
-          />
-        </div>
-        <nav className="hidden md:flex md:gap-x-11 md:text-sm md:font-semibold md:leading-6 md:text-gray-700">
-          {navigation.map((item, itemIdx) => (
-            <Link key={itemIdx} href={item.href}>
-              {item.name}
-            </Link>
-          ))}
-        </nav>
-        <div className="flex flex-1 items-center justify-end gap-x-8">
-          <button
-            type="button"
-            className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
-          >
-            <span className="sr-only">View notifications</span>
-            <BellIcon aria-hidden="true" className="h-6 w-6" />
-          </button>
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your profile</span>
+    <header className="absolute inset-x-0 top-0 z-50">
+      {!!window?.localStorage.getItem("completedOnboardingSteps") ? (
+        <button
+          className="h-8 w-full bg-red-500 text-center text-sm font-semibold leading-6 text-white transition hover:bg-red-500/90"
+          onClick={() => {
+            resetCompletedSteps();
+
+            window.location.reload();
+          }}
+        >
+          Reset onboarding tour
+        </button>
+      ) : null}
+
+      <div className="flex h-16 border-b border-gray-900/10">
+        <div className="mx-auto flex w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+          <div className="flex flex-1 items-center gap-x-6">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-3 p-3 md:hidden"
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="h-5 w-5 text-gray-900" />
+            </button>
             <img
-              alt=""
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              className="h-8 w-8 rounded-full bg-gray-800"
+              alt="Your Company"
+              src="https://tailwindui.com/img/logos/mark.svg?color=violet&shade=600"
+              className="h-8 w-auto"
             />
-          </a>
+          </div>
+          <nav className="hidden md:flex md:gap-x-11 md:text-sm md:font-semibold md:leading-6 md:text-gray-700">
+            {navigation.map((item, itemIdx) => (
+              <Link key={itemIdx} href={item.href}>
+                {item.name}
+              </Link>
+            ))}
+          </nav>
+          <div className="flex flex-1 items-center justify-end gap-x-8">
+            <button
+              type="button"
+              className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
+            >
+              <span className="sr-only">View notifications</span>
+              <BellIcon aria-hidden="true" className="h-6 w-6" />
+            </button>
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your profile</span>
+              <img
+                alt=""
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                className="h-8 w-8 rounded-full bg-gray-800"
+              />
+            </a>
+          </div>
         </div>
       </div>
       <Dialog
